058375ecf7d8823fdd91220a777597b83c2fecf8
crystal <alexey.mitskevich94@gmail.com>
2018-05-27 06:56:52 +0800

    WIP - added DataService for login structure

---
+++ src/app/data.service.ts
@@ -0,0 +1,56 @@
+import { Injectable } from '@angular/core';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { Observable } from 'rxjs/Observable';
+import { of } from 'rxjs/observable/of';
+import { catchError, map, tap } from 'rxjs/operators';
+
+import { LoginData } from './data';
+import { LoginPage } from '../pages/login/login';
+ 
+const httpOptions = {
+  headers: new HttpHeaders({ 'Content-Type': 'application/json' })
+};
+
+@Injectable()
+export class DataService {
+
+  private apiUrl = 'http://13.56.148.150:5000/api/User/Login';
+
+  constructor(private http: HttpClient) { }
+
+  // login():Observable<LoginPage[]>{
+  //   return this._http.get(apiUrl).map(r => <LoginPage[]>r.json());
+  // }
+
+  login (data: LoginData): Observable<LoginData> {
+    return this.http.post<LoginData>(this.apiUrl, data, httpOptions).pipe(
+      tap(_ => this.log(`login success`)),
+      catchError(this.handleError<LoginData>('login'))
+    );
+  }
+  
+  /**
+   * Handle Http operation that failed.
+   * Let the app continue.
+   * @param operation - name of the operation that failed
+   * @param result - optional value to return as the observable result
+   */
+  private handleError<T> (operation = 'operation', result?: T) {
+    return (error: any): Observable<T> => {
+
+      // TODO: send the error to remote logging infrastructure
+      console.error(error); // log to console instead
+
+      // TODO: better job of transforming error for user consumption
+      this.log(`${operation} failed: ${error.message}`);
+
+      // Let the app keep running by returning an empty result.
+      return of(result as T);
+    };
+  }
+
+  /** Log a HeroService message with the MessageService */
+  private log(message: string) {
+    return message;
+  }
+}
---
+++ src/app/data.ts
@@ -0,0 +1,5 @@
+export class LoginData {
+  siteName: string;
+  login: string;
+  password: string;
+}
