3a4de4596ba4627eb80a205983995bade4e05150
crystal <alexey.mitskevich94@gmail.com>
2018-05-26 23:12:54 +0800

    WIP - added form validation on login page

--- src/pages/login/login.ts
+++ src/pages/login/login.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Validators, FormBuilder, FormGroup, FormControl } from '@angular/forms';
 import { IonicPage, NavController, NavParams } from 'ionic-angular';
 import { WelcomePage } from '../welcome/welcome';
 import { ForgotPage } from '../forgot/forgot';
@@ -16,9 +17,31 @@ import { SignupPage } from '../signup/signup';
   selector: 'page-login',
   templateUrl: 'login.html',
 })
-export class LoginPage {
 
-  constructor(public navCtrl: NavController, public navParams: NavParams) {
+export class LoginPage implements OnInit {
+  loginform : FormGroup;
+  userData = { "email": "", "password": "" };
+
+  // Validation error messages that will be displayed for each form field with errors.
+  validation_messages = {
+    'email': [
+      { type: 'pattern', message: 'Please enter a number like 12345678/00.' }
+    ]
+  }
+
+  constructor(public navCtrl: NavController, public navParams: NavParams, formBuilder: FormBuilder) {
+    // Create the form and define fields and validators.
+    this.loginform = formBuilder.group({
+      loginform: ['', Validators.pattern('[0-9]{8}/[0-9]{2}')]
+    });
+  }
+
+  ngOnInit() {
+    let EMAILPATTERN = /^[a-z0-9!#$%&'*+\/=?^_`{|}~.-]+@[a-z0-9]([a-z0-9-]*[a-z0-9])?(\.[a-z0-9]([a-z0-9-]*[a-z0-9])?)*$/i;
+    this.loginform = new FormGroup({
+      email: new FormControl('', [Validators.required, Validators.pattern(EMAILPATTERN)]),
+      password: new FormControl('', [Validators.required, Validators.minLength(6)]),
+    });
   }
 
   ionViewDidLoad() {
@@ -34,7 +57,7 @@ export class LoginPage {
   }
 
   forgot(){
-    this.navCtrl.push(WelcomePage);
+    this.navCtrl.push(ForgotPage);
   }
 
 }
