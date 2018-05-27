e28a864defe36f556502de736342eddfed3358c5
crystal <alexey.mitskevich94@gmail.com>
2018-05-27 22:05:28 +0800

    WIP - removed gabage code

--- src/app/data.service.ts
+++ src/app/data.service.ts
@@ -2,10 +2,9 @@ import { Injectable } from '@angular/core';
 import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Observable } from 'rxjs/Observable';
 import { of } from 'rxjs/observable/of';
-import { catchError, map, tap } from 'rxjs/operators';
+import { catchError, tap } from 'rxjs/operators';
 
 import { LoginData } from './data';
-import { LoginPage } from '../pages/login/login';
  
 const httpOptions = {
   headers: new HttpHeaders({ 'Content-Type': 'application/json' })
